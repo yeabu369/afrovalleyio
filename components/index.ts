9a3e170b64d3b1e1beea22515614be2d9643ae4e
Yeabsra Hailu <yeabu.hailu@gmail.com>
2022-10-04 23:19:48 +0000

    Created Login Component

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from './Header/Header';
 export { default as Layout } from './Layout/Layout';
 export { default as Hero } from './Hero/Hero';
 export { default as Footer } from './Footer/Footer';
+export { default as Login } from './Login/Login';
