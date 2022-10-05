5f8fcb4e963bca32a7c0f30c4ed31f4612b61581
Yeabsra Hailu <yeabu.hailu@gmail.com>
2022-10-05 00:07:05 +0000

    Added Login and SignUp Components to Auth Page

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { Login, Signup } from '../../components';
 
-const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
+const Auth: NextPage = () => {
+  const router = useRouter();
+
+  if (router.pathname === '/auth/login') {
+    return <Login />
+  } else if (router.pathname === '/auth/signup') {
+    return <Signup />
+  } else {
+    router.push('/auth/login');
+    return <>Redirecting</>;
+  }
 }
 
 export default Auth
