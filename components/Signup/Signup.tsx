a0a3a00b0f037cf08c15cd8afa54423bde0704eb
Yeabsra Hailu <yeabu.hailu@gmail.com>
2022-10-05 20:15:05 +0000

    feat: Made Email Input autocomplete component

--- components/Signup/Signup.tsx
+++ components/Signup/Signup.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Stepper, Button, Group, Box, Container, Center, Title, Text, Paper, TextInput, PasswordInput, Stack } from '@mantine/core';
+import { useRef, useState } from 'react';
+import { Stepper, Button, Group, Box, Container, Center, Title, Text, Paper, TextInput, PasswordInput, Stack, Autocomplete, Loader } from '@mantine/core';
 import { IconMailOpened, IconShieldCheck, IconUserCheck } from '@tabler/icons';
 import AfroValleyLogo from '../Logo/Logo';
 
@@ -7,6 +7,27 @@ export default function Signup() {
   const [active, setActive] = useState(0);
   const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
+
+  const timeoutRef = useRef<number>(-1);
+  const [value, setValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<string[]>([]);
+
+  const handleChange = (val: string) => {
+    window.clearTimeout(timeoutRef.current);
+    setValue(val);
+    setData([]);
+
+    if (val.trim().length === 0 || val.includes('@')) {
+      setLoading(false);
+    } else {
+      setLoading(true);
+      timeoutRef.current = window.setTimeout(() => {
+        setLoading(false);
+        setData(['gmail.com', 'outlook.com', 'yahoo.com', 'afrovalley.io'].map((provider) => `${val}@${provider}`));
+      }, 1000);
+    }
+  };
 
   return (
     <Container size={900} py={40}>
@@ -23,9 +44,19 @@ export default function Signup() {
       <Stepper active={active} onStepClick={setActive} breakpoint="sm">
         <Stepper.Step icon={<IconUserCheck size={18} />} label="First step" description="Create an account">
           <Stack align="center">
-            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                <TextInput label="Email" placeholder="you@afrovalley.io" required />
-                <PasswordInput label="Password" placeholder="Your password" required mt="md" />
+            <Paper withBorder shadow="md" p={30} mt={30} radius="md" >
+              <TextInput label="Firstname" placeholder="First Name" required />
+              <TextInput label="Lastname" placeholder="Last Name" required mt="md" />
+              <Autocomplete
+                value={value}
+                data={data}
+                onChange={handleChange}
+                rightSection={loading ? <Loader size={16} /> : null}
+                label="Email"
+                placeholder="you@afrovalley.io"
+                mt="md"
+              />
+              <PasswordInput label="Password" placeholder="Your password" required mt="md" />
             </Paper>
           </Stack>
         </Stepper.Step>
