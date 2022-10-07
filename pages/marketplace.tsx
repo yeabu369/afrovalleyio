85975e9adc82a5366cbd406621b4be0785a0eed0
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-07 23:12:26 +0300

    feat: Created Marketplace Page

---
+++ pages/marketplace.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { Footer, Header, Layout, Market } from '../components'
+import content from '../data/footer'
+
+const Marketplace: NextPage & { getLayout: Function } = () => {
+  return (
+    <div>
+      <Head>
+        <title>Marketplace | AfroValley Trade Platform</title>
+        <meta name="description" content="Afrovalley Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Market />
+    </div>
+  )
+}
+
+Marketplace.getLayout = function getLayout(page: NextPage) {
+  return (
+    <Layout>
+      <Header />
+      {page}
+      <Footer data={content.data} />
+    </Layout>
+  )
+}
+
+export default Marketplace
