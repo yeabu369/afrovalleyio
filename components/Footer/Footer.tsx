e0f9ed240a6db1468a0103e7e249db6f8bdec810
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-09 22:32:44 +0300

    fix: linting

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,14 +4,12 @@ import AfroValleyLogo from '../Logo/Logo';
 
 const useStyles = createStyles((theme) => ({
     footer: {
-        marginTop: 120,
         paddingTop: theme.spacing.xl * 2,
         paddingBottom: theme.spacing.xl * 2,
         backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-            }`,
+        borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]}`,
     },
-
+    
     logo: {
         maxWidth: 200,
 
