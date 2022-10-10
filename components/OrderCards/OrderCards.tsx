85e850b56e265de5a54eec27c07e98afdddc0a6c
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-10 21:38:57 +0300

    feat: Added medium spacing between cards

--- components/OrderCards/OrderCards.tsx
+++ components/OrderCards/OrderCards.tsx
@@ -1,12 +1,13 @@
+import { Stack } from '@mantine/core'
 import React from 'react'
 import OrderCard from './OrderCard/OrderCard'
 
 const OrderCards = () => {
   return (
-    <>
+    <Stack spacing="md">
       <OrderCard />
       <OrderCard />
-    </>
+    </Stack>
   )
 }
 
