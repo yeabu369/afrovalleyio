8a30627ca4b805c8963027e232166bf7c46a2bbf
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-13 18:21:27 +0300

    Added Componentized Form Fields

--- components/Signup/Signup.tsx
+++ components/Signup/Signup.tsx
@@ -1,33 +1,36 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { Stepper, Button, Group, Box, Container, Center, Title, Text, Paper, TextInput, PasswordInput, Stack, Autocomplete, Loader } from '@mantine/core';
 import { IconMailOpened, IconSettings, IconShieldCheck, IconUserCheck } from '@tabler/icons';
 import AfroValleyLogo from '../Logo/Logo';
+import SignupForm from '../Forms/SignupForm/SignupForm';
+import VerificationForm from '../Forms/VerificationForm/VerificationForm';
+import { useForm } from '@mantine/form';
+import PreferenceForm from '../Forms/PreferenceForm/PreferenceForm';
 
 export default function GetStarted() {
   const [active, setActive] = useState(0);
   const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
-  const timeoutRef = useRef<number>(-1);
-  const [value, setValue] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<string[]>([]);
+  const form = useForm({
+    initialValues: {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      preference: '',
+      termsOfService: false,
+    },
 
-  const handleChange = (val: string) => {
-    window.clearTimeout(timeoutRef.current);
-    setValue(val);
-    setData([]);
-
-    if (val.trim().length === 0 || val.includes('@')) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-      timeoutRef.current = window.setTimeout(() => {
-        setLoading(false);
-        setData(['gmail.com', 'outlook.com', 'afrovalley.io'].map((provider) => `${val}@${provider}`));
-      }, 1000);
-    }
-  };
+    validate: {
+      firstname: (value: any) => { if (!value) return 'First name is required'; },
+      lastname: (value: any) => { if (!value) return 'Last name is required'; },
+      email: (value: any) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      password: (value: any) => { if (!value) return 'Password is required'; },
+      preference: (value: any) => { if (!value) return 'Preference is required'; },
+      termsOfService: (value: any) => { if (!value) return 'You must agree to the terms of service'; },
+    },
+  });
 
   return (
     <Container size={900} py={40}>
@@ -41,87 +44,18 @@ export default function GetStarted() {
       >
         Get Started
       </Title>
-      <Stepper active={active} onStepClick={setActive} breakpoint="sm">
-        <Stepper.Step icon={<IconUserCheck size={18} />} label="First step" description="Create an account">
-          <Box
-            sx={(theme) => ({
-              backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-              paddingLeft: (theme.spacing.xl) * 10,
-              paddingRight: (theme.spacing.xl) * 10,
-              paddingTop: (theme.spacing.xl) * 2,
-              paddingBottom: (theme.spacing.xl) * 2,
-              borderRadius: theme.radius.md,
-              cursor: 'pointer',
-
-              '&:hover': {
-                backgroundColor:
-                  theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-              },
-            })}
-          >
-            <Paper withBorder shadow="md" p={30} radius="md" >
-              <TextInput label="Firstname" placeholder="First Name" required />
-              <TextInput label="Lastname" placeholder="Last Name" required mt="md" />
-              <Autocomplete
-                value={value}
-                data={data}
-                onChange={handleChange}
-                rightSection={loading ? <Loader size={16} /> : null}
-                label="Email"
-                placeholder="you@afrovalley.io"
-                mt="md"
-                required
-              />
-              <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-            </Paper>
-          </Box>
+      <Stepper size="xs" active={active} onStepClick={setActive} breakpoint="sm">
+        <Stepper.Step icon={<IconUserCheck size={18} />} label="First step" description="Create an account" allowStepSelect={active > 0}>
+          <SignupForm form={form} />
         </Stepper.Step>
-        <Stepper.Step icon={<IconMailOpened size={18} />} label="Second step" description="Verify email">
-          <Box
-            sx={(theme) => ({
-              backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-              paddingLeft: (theme.spacing.xl) * 10,
-              paddingRight: (theme.spacing.xl) * 10,
-              paddingTop: (theme.spacing.xl) * 2,
-              paddingBottom: (theme.spacing.xl) * 2,
-              borderRadius: theme.radius.md,
-              cursor: 'pointer',
-
-              '&:hover': {
-                backgroundColor:
-                  theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-              },
-            })}
-          >
-            <Paper withBorder shadow="md" p={30} radius="md" >
-              <Text size="md" weight={500} mb="md">
-                We have sent a verification code to your email address. Please enter the code below to verify your email address.
-              </Text>
-              <TextInput label="Verification Code" placeholder="Verification Code" required />
-            </Paper>
-          </Box>
+        <Stepper.Step icon={<IconMailOpened size={18} />} label="Second step" description="Verify email" allowStepSelect={active > 1}>
+          <VerificationForm form={form} />
         </Stepper.Step>
-        <Stepper.Step icon={<IconSettings size={18} />} label="Third step" description="Update Preferences">
-          <Box
-            sx={(theme) => ({
-              backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-              paddingLeft: (theme.spacing.xl) * 10,
-              paddingRight: (theme.spacing.xl) * 10,
-              paddingTop: (theme.spacing.xl) * 2,
-              paddingBottom: (theme.spacing.xl) * 2,
-              borderRadius: theme.radius.md,
-              cursor: 'pointer',
-
-              '&:hover': {
-                backgroundColor:
-                  theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-              },
-            })}
-          >
-          </Box>
+        <Stepper.Step icon={<IconSettings size={18} />} label="Third step" description="Update Preferences" allowStepSelect={active > 2}>
+          <PreferenceForm form={form} />
         </Stepper.Step>
-        <Stepper.Step icon={<IconShieldCheck size={18} />} label="Final step" description="Get full access">
-        <Box
+        <Stepper.Step icon={<IconShieldCheck size={18} />} label="Final step" description="Get full access" allowStepSelect={active > 3}>
+          <Box
             sx={(theme) => ({
               backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
               paddingLeft: (theme.spacing.xl) * 10,
@@ -151,7 +85,10 @@ export default function GetStarted() {
 
       <Group position="center" mt="xl">
         <Button variant="default" onClick={prevStep}>Back</Button>
-        <Button variant="gradient" gradient={{ from: 'green', to: 'cyan' }} onClick={nextStep}>Next step</Button>
+        {(active === 3) ? (
+          <Button type="submit" variant="gradient" gradient={{ from: 'green', to: 'cyan' }} onClick={nextStep}>Submit</Button>) :
+          <Button variant="gradient" gradient={{ from: 'green', to: 'cyan' }} onClick={nextStep}>Next step</Button>
+        }
       </Group>
     </Container >
   );
