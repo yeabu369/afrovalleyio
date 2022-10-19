ba1a6939f6bfbbd35e8c3d1b41dd387b325b8c0c
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-19 19:29:59 +0300

    feat: Created Order Page

---
+++ pages/orders/[id].tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { Footer, Header, Layout, Market, OrderContainer } from '../../components'
+import content from '../../data/footer'
+
+const Order: NextPage & { getLayout: Function } = () => {
+  return (
+    <>
+      <Head>
+        <title>Order | AfroValley Trade Platform</title>
+        <meta name="description" content="Afrovalley Order" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <OrderContainer />
+    </>
+  )
+}
+
+Order.getLayout = function getLayout(page: NextPage) {
+  return (
+    <Layout>
+      <Header />
+      {page}
+      <Footer data={content.data} />
+    </Layout>
+  )
+}
+
+export default Order
