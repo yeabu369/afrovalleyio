03d9067f879cabadeb6b900c3eb8eb7ecea8938b
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-19 19:39:13 +0300

    Created Order Card stack style

--- components/OrderCards/OrderCards.styles.ts
+++ components/OrderCards/OrderCards.styles.ts
@@ -1,1 +1,9 @@
-export {}
+import { createStyles } from "@mantine/core";
+
+export default createStyles((theme) => ({
+    stack: {
+        border: `2px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]}`,
+        height: "100%",
+        overFlowY: "hidden"
+    },
+}));
