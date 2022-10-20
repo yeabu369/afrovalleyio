847d18960884416c1dfd3c060b806dde37714d82
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-20 16:12:38 +0300

    feat: Created CropShowcase Component

---
+++ components/OrderContainer/CropShowcase/CropShowcase.styles.ts
@@ -0,0 +1,9 @@
+import { createStyles } from "@mantine/core";
+
+export default createStyles((theme) => ({
+    image: {
+        "& img": {
+            borderRadius: theme.radius.md,
+        }
+    }
+}));
