1acd03c69a96e847ba0086ecdc734667166ec0c9
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-10-27 08:49:02 -0400

    refactor: Customized Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -10,105 +10,7 @@ import {
   IconSettings,
 } from '@tabler/icons';
 import AfroValleyLogo from '../Logo/Logo';
-
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    display: 'flex',
-  },
-
-  aside: {
-    flex: '0 0 60px',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    borderRight: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
-  },
-
-  main: {
-    flex: 1,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-  },
-
-  mainLink: {
-    width: 44,
-    height: 44,
-    borderRadius: theme.radius.md,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
-    },
-  },
-
-  mainLinkActive: {
-    '&, &:hover': {
-      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
-      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
-    },
-  },
-
-  title: {
-    boxSizing: 'border-box',
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    marginBottom: theme.spacing.xl,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-    padding: theme.spacing.md,
-    paddingTop: 18,
-    height: 60,
-    borderBottom: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
-  },
-
-  logo: {
-    boxSizing: 'border-box',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    height: 60,
-    paddingTop: theme.spacing.md,
-    borderBottom: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
-    marginBottom: theme.spacing.xl,
-  },
-
-  link: {
-    boxSizing: 'border-box',
-    display: 'block',
-    textDecoration: 'none',
-    borderTopRightRadius: theme.radius.md,
-    borderBottomRightRadius: theme.radius.md,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-    padding: `0 ${theme.spacing.md}px`,
-    fontSize: theme.fontSizes.sm,
-    marginRight: theme.spacing.md,
-    fontWeight: 500,
-    height: 44,
-    lineHeight: '44px',
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    },
-  },
-
-  linkActive: {
-    '&, &:hover': {
-      borderLeftColor: theme.fn.variant({ variant: 'filled', color: theme.primaryColor })
-        .background,
-      backgroundColor: theme.fn.variant({ variant: 'filled', color: theme.primaryColor })
-        .background,
-      color: theme.white,
-    },
-  },
-}));
+import useStyles from './Navbar.styles';
 
 const mainLinksMockdata = [
   { icon: IconHome2, label: 'Home' },
@@ -121,17 +23,12 @@ const mainLinksMockdata = [
 ];
 
 const linksMockdata = [
+  'Dashboard',
+  'Orders',
+  'Account',
   'Security',
   'Settings',
-  'Dashboard',
-  'Releases',
-  'Account',
-  'Orders',
   'Clients',
-  'Databases',
-  'Pull Requests',
-  'Open Issues',
-  'Wiki pages',
 ];
 
 export default function DoubleNavbar() {
@@ -165,11 +62,11 @@ export default function DoubleNavbar() {
   ));
 
   return (
-    <Navbar height={750} width={{ sm: 300 }}>
+    <Navbar height="100ch" width={{ sm: 300 }}>
       <Navbar.Section grow className={classes.wrapper}>
         <div className={classes.aside}>
           <div className={classes.logo}>
-            <AfroValleyLogo width={30} height={10} />
+            <AfroValleyLogo width={60} height={20} />
           </div>
           {mainLinks}
         </div>
