dd2f97fb0919f202f7aead7e1d286a05a13f5e99
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-11-05 12:24:24 -0500

    chore: remove unused imports

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 import { Footer, Header, Hero, Layout, Navbar } from '../components'
 import content from '../data/footer';
 
 const Dashboard: NextPage & { getLayout: Function } = () => {
+
   return (
     <div>
       <Head>
