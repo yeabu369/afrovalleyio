0de2a292b90db0dd1d459982fc6ebc2ac1f91893
henok <henokcodes@gmail.com>
2022-11-19 23:38:55 -0600

    Clean Up

--- components/OrderCards/OrderCards.styles.ts
+++
@@ -1,9 +0,0 @@
-import { createStyles } from "@mantine/core";
-
-export default createStyles((theme) => ({
-    stack: {
-        border: `2px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]}`,
-        height: "100%",
-        overFlowY: "scroll"
-    },
-}));
--- components/OrderContainer/CropShowcase/CropShowcase.styles.ts
+++
@@ -1,9 +0,0 @@
-import { createStyles } from "@mantine/core";
-
-export default createStyles((theme) => ({
-    image: {
-        "& img": {
-            borderRadius: theme.radius.md,
-        }
-    }
-}));
